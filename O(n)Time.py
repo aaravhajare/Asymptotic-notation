d1b6255d66a7a25106bf36073386c361fe4727f0
aaravhajare <hajareaarav66@gmail.com>
2025-07-20 09:38:24 +0530

    upadte

---
+++ O(n)Time.py
@@ -0,0 +1,13 @@
+
+def print_n(n):
+
+    iter = 0 
+
+    for i in range(1,n+1):
+        iter += 1 
+        print("n.o of iteration = n" , n)
+
+print_n(10)
+print_n(1)
+print_n(6)
+print("with n time taken will increase")
