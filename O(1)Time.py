f98e1a7df141381d4ec249a9ba572e1df3c70af7
aaravhajare <hajareaarav66@gmail.com>
2025-07-20 09:33:43 +0530

    update

---
+++ O(1)Time.py
@@ -0,0 +1,12 @@
+
+def print_n(n) :
+
+    iter = 0
+
+    print(n)
+    iter += 1
+    print("total iterations  :" , iter)
+
+print_n(10)
+print_n(20)
+print("if there is any value in in the time and n.o of iteration won't change")
